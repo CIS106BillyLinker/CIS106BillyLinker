7447dd81ba5f90e2091d4a5d10364c588477ba05
Billy Linker <lw40934@mail.harpercollege.edu>
2024-10-10 20:02:05 -0500

    Delete PS4/PS4P4.py

--- PS4/PS4P4.py
+++
@@ -1,5 +0,0 @@
-appliancename = input("Enter the name of the appliance: ")
-appliancecost = float(input("Enter the cost of the appliance: "))
-warrantee = 0.1*appliancecost if appliancecost > 1000 else 0.05*appliancecost
-total = appliancecost+warrantee
-print(f"The name of the appliance is {appliancename} The cost of the appliance is ${appliancecost:.2f} The warrantee is ${warrantee} The total is ${total:.2f}")
